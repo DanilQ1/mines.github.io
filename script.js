5b8aeb5c1a26ea6a012f6218f897f58a01c6c5cb
DanilQ1 <143510461+DanilQ1@users.noreply.github.com>
2024-02-26 20:20:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('flipButton').addEventListener('click', async function() {
+  const loading = document.getElementById('loading');
+  const photoContainer = document.getElementById('photoContainer');
+  photoContainer.style.display = 'none';
+  loading.style.display = 'block';
+  
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  const randomPhotoNumber = Math.floor(Math.random() * 96) + 1;
+  photoContainer.style.backgroundImage = `url('photos/${randomPhotoNumber}.jpg')`;
+  photoContainer.style.display = 'block';
+  loading.style.display = 'none';
+});
+
+
+
+
